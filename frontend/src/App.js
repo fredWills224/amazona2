3e2bd7a3a639622a28ec52fc434f95772620e05e
fredWills224 <fredwills4229@gmail.com>
2020-12-13 15:07:17 -0500

    init commit after 12-13-2020-pull

---
+++ frontend/src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import data from './data';
+import Product from './components/Product';
+
+function App() {
+  return (
+    <div>
+      <div className="grid-container">
+
+        <header className="row">
+          <div>
+            <a className="brand" href="/">amazona1</a>
+          </div>
+          <div>
+            <a href="/cart">Cart</a>
+            <a href="/signin">Sign In</a>
+          </div>
+        </header>
+
+        <main>
+          <div className="row center">
+            {
+              data.products.map((product) => (
+                
+                <Product key={product._id} product={product}></Product>
+
+              ))
+            }
+          </div>
+        </main>
+
+        <footer className="row center">
+          All rights reserved
+        </footer>
+      
+      </div>
+    </div>
+  );
+}
+
+export default App;
