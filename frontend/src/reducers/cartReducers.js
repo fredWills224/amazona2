f67c589f67eed0c7447934ba3c602a5e4b598791
fredWills224 <fredwills4229@gmail.com>
2020-12-20 01:03:16 -0500

    add to cart action

---
+++ frontend/src/reducers/cartReducers.js
@@ -0,0 +1,35 @@
+import { CART_ADD_ITEM } from "../constants/cartConstants";
+
+export const cartReducer = (state = { cartItems:[] }, action)=>{
+    
+    switch(action.type){
+        case CART_ADD_ITEM:
+        
+            const item = action.payload;
+            const existItem = state.cartItems.find(x => x.product === item.product);
+            /* updating item if it already exist in array */
+            /* 
+                keep current state of all elements in [cartItems] array. Assign the product
+                from payload to this variable [x]. Check to see if [x] has an id that matches any of
+                the elements that are currently in the array. If it does replace the element
+                that already exist in the array with [x]. Return [x] as default payload product.
+            */
+            if(existItem){
+                return{
+                    ...state,
+                    cartItems: state.cartItems.map( x =>
+                        x.product === existItem.product 
+                        ? item : x 
+                    )
+                }
+            }else{
+                return {...state, cartItems: [...state.cartItems, item]};
+            }
+            
+        ;
+        default:
+            return state
+        ;
+    }
+
+}
