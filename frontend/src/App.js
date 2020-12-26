f9b02c4e353d7bdc4a051013a805d23f92a28ea4
fredWills224 <fredwills4229@gmail.com>
2020-12-26 13:03:50 -0500

    display orders history

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Link, Route } from 'react-router-dom';
 import { signout } from './actions/userActions';
 import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
+import OrderHistoryScreen from './screens/OrderHistoryScreen';
 import OrderScreen from './screens/OrderScreen';
 import PaymentMethodScreen from './screens/PaymentMethodScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
@@ -28,10 +29,12 @@ function App() {
       <div className="grid-container">
 
         <header className="row">
+          
           <div>
             <Link className="brand" to="/">amazona2</Link>
           </div>
           <div>
+
             <Link to="/cart">
               Cart
               {cartItems.length >0 && (
@@ -41,22 +44,30 @@ function App() {
             {
               userInfo ? (
                 <div className="dropdown">
+
                   <Link to="#">
                     {userInfo.name} <i className="fa fa-caret-down"></i>
                   </Link>
                   <ul className="dropdown-content">
-                    <Link to="#signout" onClick={signoutHandler}>
-                      SignOut
-                    </Link>
+                    <li>
+                      <Link to="/orderhistory">Order History</Link>
+                    </li>
+                    <li>
+                      <Link to="#signout" onClick={signoutHandler}>
+                        SignOut
+                      </Link>
+                    </li>
                   </ul>
+
                 </div>
               ) : (
                 <Link to="/signin">Sign In</Link>
               )
             }
-          </div>
-        </header>
 
+          </div>
+
+        </header>
         <main>
           <Route path="/cart/:id?" component={CartScreen}></Route>
           <Route path="/product/:id" component={ProductScreen}></Route>
@@ -66,9 +77,9 @@ function App() {
           <Route path="/payment" component={PaymentMethodScreen}></Route>
           <Route path="/placeorder" component={PlaceOrderScreen}></Route>
           <Route path="/order/:id" component={OrderScreen}></Route>
+          <Route path="/orderhistory" component={OrderHistoryScreen}></Route>
           <Route path="/" component={HomeScreen} exact></Route>          
         </main>
-
         <footer className="row center">
           All rights reserved
         </footer>
