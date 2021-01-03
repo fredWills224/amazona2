68d65958a1ab96708a1877b8cc801a0ee667154b
fredWills224 <fredwills4229@gmail.com>
2021-01-03 14:58:53 -0500

    delete orders

--- frontend/src/screens/OrderListScreen.js
+++ frontend/src/screens/OrderListScreen.js
@@ -1,20 +1,26 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
-import { listOrders } from '../actions/orderActions';
+import { deleteOrder, listOrders } from '../actions/orderActions';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
+import { ORDER_DELETE_RESET } from '../constants/orderConstants';
 
 export default function OrderListScreen(props) {
     
     const orderList = useSelector(state => state.orderList);
     const { loading, error, orders } = orderList;
+    const orderDelete = useSelector(state => state.orderDelete);
+    const { loading: loadingDelete, error: errorDelete, success: successDelete } = orderDelete;
     const dispatch = useDispatch();
     useEffect(() => {
+        dispatch({ type: ORDER_DELETE_RESET });
         dispatch(listOrders());
-    }, [dispatch]);
+    }, [dispatch, successDelete]);
 
     const deleteHandler = (order)=>{
-        // TODO:delete handler
+        if(window.confirm('Are you sure that you want to delete this order?')){
+            dispatch(deleteOrder(order._id));
+        }
     }
 
     return (
@@ -22,6 +28,8 @@ export default function OrderListScreen(props) {
         <div>
             
             <h1>Orders</h1>
+            {loadingDelete && <LoadingBox></LoadingBox>}
+            {errorDelete && <MessageBox variant="danger">{errorDelete}</MessageBox>}
             {
                 loading ? (<LoadingBox></LoadingBox>
                 ) : error ? (
